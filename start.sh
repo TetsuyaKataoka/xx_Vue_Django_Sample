a49c62cf7e20a7dc501000cb71ad9c21cb77de58
Tetsuya Kataoka <kataoka_learning@yahoo.co.jp>
2021-05-03 17:24:39 +0900

    start.sh 削除

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sleep 5
-python serverproject/manage.py makemigrations
-python serverproject/manage.py migrate
-python serverproject/manage.py collectstatic --noinput
-uwsgi --socket :8001 --module mysite.wsgi
