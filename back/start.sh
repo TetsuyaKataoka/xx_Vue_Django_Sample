d7a5a6a07f80d97e48946451e471b2341d88edfe
Tetsuya Kataoka <kataoka_learning@yahoo.co.jp>
2021-05-03 17:24:05 +0900

    commit

--- back/start.sh
+++ back/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sleep 5
+# マイグレーション
 python manage.py makemigrations
 python manage.py migrate
+# 静的ファイルの配置
 python manage.py collectstatic --noinput
+# uWSGIでデプロイ
 uwsgi --socket :8001 --module serverproject.wsgi
